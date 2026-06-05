f92d61841d810ec7b3f350489809ac71d8eab4b9
iSuleymanCoban <suleymanc756@gmail.com>
2026-06-05 11:33:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,29 @@ with col_btn:
             st.error("Lütfen geçerli bir görev metni girin.")
 st.write("")
 #görevlerin listesi 
+if not st.session_state.tasks:
+    st.info("Henüz görev eklenmedi. Yukarıdaki kutuya bir görev ekleyin!")
+
+for i , task  in enumerate(st.session_state.tasks):
+    c1,c2,c3 = st.columns([6, 1, 1])
+    with c1:
+        checked = st.checkbox(task["text"], value=task["done"], key=f"task_{i}", label_visibility="collapsed")
+        if checked != task["done"]:
+            st.session_state.tasks[i]["done"] = checked
+            st.rerun()
+    with c2:
+        st.markdown(
+        f"""
+        
+        <div>{task["text"]}</div>
+        
+        """,unsafe_allow_html=True
+        )
+    with c3:
+        if st.button("Sil", key=f"delete_{i}", use_container_width=True):
+            del st.session_state.tasks[i]
+            st.success("Görev başarıyla silindi!")
+            st.rerun()
+            
 
 #özet
